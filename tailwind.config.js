270da12ea146730b663cd9a8755cdc33384b320d
lordever <lordever@mail.ru>
2025-03-23 23:49:59 +0500

    Social links profile completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter', 'sans-serif']
+      },
+      colors: {
+        dark: '#141414',
+        darkGray: '#1F1F1F',
+        saladGreen: '#c4f82a',
+        btnGray: '#333333'
       }
     },
   },

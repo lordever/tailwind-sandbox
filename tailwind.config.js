431f00e3172f177402439a5911bd0334ec7e21a9
lordever <lordever@mail.ru>
2025-03-24 22:55:24 +0500

    Product list with cart setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Red Hat Text', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }

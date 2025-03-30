49a11cdba5e66c481e9aeea3ea7b88309584828d
lordever <lordever@mail.ru>
2025-03-30 10:48:13 +0500

    QR code cart component set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Outfit', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }

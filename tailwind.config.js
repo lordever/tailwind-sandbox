378f42643f782b47a551f01c5a3b921dbbc2bf07
lordever <lordever@mail.ru>
2025-03-30 11:19:03 +0500

    QR code has been completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*html"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Outfit', 'sans-serif']
-      },
+    content: ["./*html"],
+    theme: {
+        fontSize: {
+            sm: ['15px', {lineHeight: '140%', letterSpacing: '0.2px'}],
+            '2xl': ['22px', {lineHeight: '120%', letterSpacing: '0px'}]
+        },
+        extend: {
+            fontFamily: {
+                sans: ['Outfit', 'sans-serif']
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 

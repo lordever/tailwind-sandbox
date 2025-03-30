0828eb1ff64c4900454162d7bcc234fc16d5be13
lordever <lordever@mail.ru>
2025-03-30 21:47:01 +0500

    Blog card has been finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*html"],
-  theme: {
-    extend: {
-        colors: {
-            yellow: '#F4D04E'
+    content: ["./*html"],
+    theme: {
+        extend: {
+            colors: {
+                yellow: '#F4D04E'
+            },
+            fontFamily: {
+                sans: ['Figtree', 'sans-serif']
+            },
+            fontSize: {
+                sm: ['14px', {lineHeight: '150%', letterSpacing: '0px'}],
+                '2xl': ['24px', {lineHeight: '150%', letterSpacing: '0px'}],
+                'base': ['16px', {lineHeight: '150%', letterSpacing: '0px'}]
+            }
         },
-        fontFamily: {
-            sans: ['Figtree', 'sans-serif']
-        }
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 

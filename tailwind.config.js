907530db55efa238bdb57bed5287eb8db0bbba21
lordever <lordever@mail.ru>
2025-04-02 00:55:26 +0500

    Product preview card component has been completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,23 @@ module.exports = {
                 fraunces: ['Fraunces']
             },
             fontSize: {
-                'text-preset-1': ['32px', {lineHeight: '100%', letterSpacing: '0px'}],
-                'text-preset-2': ['14px', {lineHeight: '110%', letterSpacing: '0px'}],
-                'text-preset-3': ['14px', {lineHeight: '160%', letterSpacing: '0px'}],
-                'text-preset-4': ['12px', {lineHeight: '120%', letterSpacing: '5px'}],
-                'text-preset-5': ['13px', {lineHeight: '120%', letterSpacing: '0px'}],
+                'preset-1': ['32px', {lineHeight: '100%', letterSpacing: '0px'}],
+                'preset-2': ['14px', {lineHeight: '110%', letterSpacing: '0px'}],
+                'preset-3': ['14px', {lineHeight: '160%', letterSpacing: '0px'}],
+                'preset-4': ['12px', {lineHeight: '120%', letterSpacing: '5px'}],
+                'preset-5': ['13px', {lineHeight: '120%', letterSpacing: '0px'}],
+            },
+            colors: {
+                cream: '#F2EAE2',
+                gray: '#6C7289',
+                black: '#1C232B',
+                'green-500': '#3D8168',
+                'green-700': '#1A4032'
             }
         },
+        screens: {
+            md: '576px'
+        }
     },
     plugins: [],
 }

b04fe9cab9f6ce0c17324bae72bb563d5c2b2f9c
lordever <lordever@mail.ru>
2025-04-12 23:59:26 +0500

    Hero section was completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,10 @@ module.exports = {
             },
             colors: {
                 'cyan-600': '#4D96A9',
+                'cyan-400': '#71C0D4',
                 'cyan-300': '#8FE3F9',
                 'purple-600': '#855FB1',
+                'purple-400': '#B18BDD',
                 'purple-300': '#D9B8FF',
                 'slate-900': '#28283D',
                 'slate-600': '#87879D',
@@ -18,9 +20,11 @@ module.exports = {
             },
             fontSize: {
                 'preset-1': ['64px', {lineHeight: '110%', letterSpacing: 0}],
+                'tablet-preset-1': ['48px', {lineHeight: '110%', letterSpacing: 0}],
                 'preset-2': ['40px', {lineHeight: '110%', letterSpacing: 0}],
                 'preset-3': ['16px', {lineHeight: '110%', letterSpacing: '4px'}],
-                'preset-4': ['18px', {lineHeight: '110%', letterSpacing: '4px'}],
+                'preset-4': ['18px', {lineHeight: '150%', letterSpacing: 0}],
+                'tablet-preset-4': ['16px', {lineHeight: '150%', letterSpacing: 0}],
                 'preset-5': ['16px', {lineHeight: '150%', letterSpacing: 0}],
             }
         },
